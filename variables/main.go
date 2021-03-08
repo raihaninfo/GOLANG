ad55d9105d1ae4dd4cefefc6111cb7e39cf21df5
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-08 12:26:53 +0600

    int type variable

--- variables/main.go
+++ variables/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
-// string type data
 func main() {
+	// string type data
 	var name string = "Raihan"
 	fmt.Println(name)
+
+	// int type data 
+	var num1 int = 30
+	var num2 int = 40
+	var sum int = num1 + num2
+	fmt.Println(sum)
 }
+
