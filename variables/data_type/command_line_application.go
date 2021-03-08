7a5653b187d5e4e65208e4d29ea96683b0d18d81
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-08 14:08:35 +0600

    command line application

---
+++ variables/data_type/command_line_application.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// fmt.Println("Enter your name & age : ")
+	// var name string
+	// var age int
+	// fmt.Scanf("%s %d", &name, &age)
+	// fmt.Printf("your name %s & age is %d", name, age)
+
+	fmt.Println("Enter your First name : ")
+	var fname string
+	fmt.Scanln(&fname)
+
+	fmt.Println("Enter your last name : ")
+	var lname string
+	fmt.Scanln(&lname)
+
+	fmt.Print("Your full name :")
+	fmt.Print(fname + " " + lname)
+
+}
