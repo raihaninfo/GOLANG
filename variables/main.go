d7ffb74569f502145006a7047da64a972c623834
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-08 12:31:48 +0600

    2 string variable

--- variables/main.go
+++ variables/main.go
@@ -7,6 +7,11 @@ func main() {
 	var name string = "Raihan"
 	fmt.Println(name)
 
+	// 2 variable
+	var name1, city string = "raihan", "Dhaka"
+	fmt.Println(name1, city)
+
+
 	// int type data 
 	var num1 int = 30
 	var num2 int = 40
