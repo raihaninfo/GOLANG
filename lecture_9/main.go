30a9dc16af1b1f35103ed702f6af9b3c05f7760c
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-14 21:02:48 +0600

    baleen value

---
+++ lecture_9/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+var isFound bool
+fmt.Println(true && true)
+fmt.Println(true && false)
+fmt.Println(true || true)
+fmt.Println(true || false)
+fmt.Println(!frue)
+}
