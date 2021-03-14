f6472ba017db0b924a2a798bc0930b858348721e
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-14 09:37:43 +0600

    char int variable

--- variables/main.go
+++ variables/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main(){
-var num1 int = 12
-var num2 int = 12
-var sum int = num1+num2
-fmt.Println(sum)
+//var num1 int = 12
+//var num2 int = 12
+//var sum int = num1+num2
+//var name string = "Raihan"
+var c rune = 'A'
+var age int = 10
+var result float32 =20.202
+fmt.Println(c, age, result)
 }
