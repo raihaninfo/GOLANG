2e46f64d5dc045c20c79b62847d24dbc3d4e4913
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-15 21:18:34 +0600

    maps

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -4,17 +4,6 @@ import "fmt"
 
 func main() {
 	// array, slice, map, struct
-	// var student [3]string
-	// student[0] = "Raihan"
-	// student[1] = "Hasan"
-	// student[2] = "Ruhul"
-	//  x := student[0:2]
-
-	// x := make([]string, 0)
-	var x [] string
-	x = append(x, "Salman", "Farhan", "habib", "Hanas", "Ruhul")
-
-	fmt.Println(x, len(x))
-	fmt.Printf("%T", x) //data type chack 
+	fmt.Println("Raihan")
 
 }
