047e6d2d4bc7b8fa5e80f754b2ae29ceb2775bb1
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-15 21:06:52 +0600

    clice, append function added

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -4,6 +4,17 @@ import "fmt"
 
 func main() {
 	// array, slice, map, struct
-	fmt.Println("Raihan")
+	// var student [3]string
+	// student[0] = "Raihan"
+	// student[1] = "Hasan"
+	// student[2] = "Ruhul"
+	//  x := student[0:2]
+
+	// x := make([]string, 0)
+	var x [] string
+	x = append(x, "Salman", "Farhan", "habib", "Hanas", "Ruhul")
+
+	fmt.Println(x, len(x))
+	fmt.Printf("%T", x) //data type chack 
 
 }
