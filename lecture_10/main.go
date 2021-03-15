df09ace55f567902f8fe01d7cb82d3149a70ddfc
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-15 20:38:27 +0600

    array len dynamic

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -1,22 +1,22 @@
 package main
 
 import "fmt"
-func main(){
+
+func main() {
 	// array, slice, map, struct
-	var student [3] string
-	student[0]="Raihan"
-	student[1]="Hasan"
-	student[2]="Ruhul"
+	var student [3]string
+	student[0] = "Raihan"
+	student[1] = "Hasan"
+	student[2] = "Ruhul"
 	fmt.Println(student[2])
-	var age [3] int
+	var age [3]int
 	age[0] = 12
 	age[1] = 15
 	age[2] = 20
 	fmt.Println(age[2])
 	// fmt.Println(len(student))
 	// short hand way
-	students := [3] string{"Raihan", "hasan", "Ruhul-Amin"}
+	students := [...]string{"Raihan", "hasan", "Ruhul-Amin", "monirul"}
 	fmt.Println(students)
-
 
 }
