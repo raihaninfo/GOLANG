f4deaeb42635be2834425bf1f5635ca15eece590
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-15 20:28:02 +0600

    index array

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -3,7 +3,15 @@ import "fmt"
 func main(){
 	// array, slice, map, struct
 	var student [3] string
-	fmt.Println(student)
+	student[0]="Raihan"
+	student[1]="Hasan"
+	student[2]="Ruhul"
+	fmt.Println(student[2])
+	var age [3] int
+	age[0] = 12
+	age[1] = 15
+	age[2] = 20
+	fmt.Println(age[2])
 	// fmt.Println(len(student))
 
 
