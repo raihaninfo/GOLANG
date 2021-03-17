260fb993367abaacc03078fae80b7d4ff94fb1ef
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-17 15:36:15 +0600

    else if

--- lecture_12/main.go
+++ lecture_12/main.go
@@ -8,11 +8,15 @@ func main(){
 	fmt.Scanf("%d", &age)
 
 	if age <3 {
-		fmt.Println("your are infant")
+		fmt.Println("you are infant")
 	} else if age >3 && age <18{
-		fmt.Println("Your are baby")
+		fmt.Println("You are baby")
 	}else if age >18 && age <21{
-		fmt.Println("Your are adult")
+		fmt.Println("You are adult")
+	}else if age >21 && age <50{
+		fmt.Println("You are middle age")
+	}else if age >50 && age <100{
+		fmt.Println("you are Old man")
 	}
 
 }
