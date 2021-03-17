7340bee156ec8588a8126d3bb93282a7811e6d5d
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-17 15:32:37 +0600

    if else condition

--- lecture_12/main.go
+++ lecture_12/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Md Abu Raihan")
+	fmt.Print("Enter your age : ")
+	var age int
+	fmt.Scanf("%d", &age)
+
+	if age <3 {
+		fmt.Println("your are infant")
+	} else if age >3 && age <18{
+		fmt.Println("Your are baby")
+	}else if age >18 && age <21{
+		fmt.Println("Your are adult")
+	}
+
 }
