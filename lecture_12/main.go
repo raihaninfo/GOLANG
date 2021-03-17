d1fa9be9d1995b0403aac700643814963d8e48e2
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-17 15:37:11 +0600

    code format

--- lecture_12/main.go
+++ lecture_12/main.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Print("Enter your age : ")
 	var age int
 	fmt.Scanf("%d", &age)
 
-	if age <3 {
+	if age < 3 {
 		fmt.Println("you are infant")
-	} else if age >3 && age <18{
+	} else if age > 3 && age < 18 {
 		fmt.Println("You are baby")
-	}else if age >18 && age <21{
+	} else if age > 18 && age < 21 {
 		fmt.Println("You are adult")
-	}else if age >21 && age <50{
+	} else if age > 21 && age < 50 {
 		fmt.Println("You are middle age")
-	}else if age >50 && age <100{
+	} else if age > 50 && age < 100 {
 		fmt.Println("you are Old man")
 	}
 
