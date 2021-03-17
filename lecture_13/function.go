2f0fd6186c1522747dbec618d26f952d1f6ee2c6
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-17 20:51:05 +0600

    golang function

---
+++ lecture_13/function.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/*func add(x int, y int) int{
+	// function body
+	r := x+y
+	return r
+
+}
+/*
+func add(x, y int) int{
+	// function body
+	r := x+y
+	return r
+
+ }*/
+func add(x, y int)  int{
+	// function body
+	r := x+y
+	return r
+
+}
+
+func main(){
+	// code
+
+	x:= add(20, 20)
+	fmt.Println(x)
+}
