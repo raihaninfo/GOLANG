67f2ed5e437b3ec92a686683eef575fdd0e6b06f
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-17 19:32:11 +0600

    code format

--- lecture_12/forloop.go
+++ lecture_12/forloop.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	// for loop
 
-	for i := 1; i <=10; i++ {
+	for i := 1; i <= 10; i++ {
 		fmt.Println(i)
 	}
 
 	students := []string{"Raihan", "hasan", "Ruhul-amin"}
-	for i, std := range students{
+	for i, std := range students {
 		fmt.Println(i, std)
 	}
 
-	i:=0
-	for i<10 {
+	i := 0
+	for i < 10 {
 		fmt.Println("raihan")
 		i++
 	}
-	
+
 }
