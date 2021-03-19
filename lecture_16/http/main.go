6de198f72e2f935c54e0727b53daf64cdd810c72
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-19 16:35:00 +0600

    code format

--- lecture_16/http/main.go
+++ lecture_16/http/main.go
@@ -5,35 +5,36 @@ import (
 	"net"
 	"os"
 )
-func main(){
-	nl,err:=net.Listen("tcp", ":8888")
-	if err!= nil{
+
+func main() {
+	nl, err := net.Listen("tcp", ":8888")
+	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 	conn, err := nl.Accept()
-	if err!=nil{
+	if err != nil {
 		fmt.Println(err.Error())
 	}
-	bs:=make([]byte,1024)
+	bs := make([]byte, 1024)
 
-	n, e:=conn.Read(bs)
-	if e!=nil{
+	n, e := conn.Read(bs)
+	if e != nil {
 		fmt.Println(e.Error())
 	}
 	fmt.Println(n)
-	reqstr:=string(bs)
+	reqstr := string(bs)
 	fmt.Println(reqstr)
 
 	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>This is my first golang web<br>
 	<h2>Md Abu Raihan</h2></strong></body></html>`
 
-	resp:="HTTP/1.1 200 OK\r\n" +
-	"Content-Length: %d\r\n" +
-	"Content-Type: text/html\r\n" +
-	"\r\n%s"
+	resp := "HTTP/1.1 200 OK\r\n" +
+		"Content-Length: %d\r\n" +
+		"Content-Type: text/html\r\n" +
+		"\r\n%s"
 
-	msg:= fmt.Sprintf(resp, len(body), body)
+	msg := fmt.Sprintf(resp, len(body), body)
 	fmt.Println(msg)
 	conn.Write([]byte(msg))
 
