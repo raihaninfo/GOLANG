acc63aa3552d9faa8087f3456c55d21898889a9c
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-19 16:00:57 +0600

    http and net package

---
+++ lecture_16/http/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+func main(){
+	nl,err:=net.Listen("tcp", ":8080")
+	if err!= nil{
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	conn, err := nl.Accept()
+	if err!=nil{
+		fmt.Println(err.Error())
+	}
+	bs:=make([]byte,1024)
+
+	n, e:=conn.Read(bs)
+	if e!=nil{
+		fmt.Println(e.Error())
+	}
+	fmt.Println(n)
+	reqstr:=string(bs)
+	fmt.Println(reqstr)
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>This is my first golang web<br>
+	<h2>Md Abu Raihan</h2></strong></body></html>`
+
+	resp:="HTTP/1.1 200 OK\r\n" +
+	"Content-Length: %d\r\n" +
+	"Content-Type: text/html\r\n" +
+	"\r\n%s"
+
+	msg:= fmt.Sprintf(resp, len(body), body)
+	fmt.Println(msg)
+	conn.Write([]byte(msg))
+
+	//conn.Close()
+
+}
