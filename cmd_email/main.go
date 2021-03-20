a60b15cb0c638bf74366c4e3eb6ca09e77a5462e
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-20 19:32:27 +0600

    code change

--- cmd_email/main.go
+++ cmd_email/main.go
@@ -7,21 +7,9 @@ import (
 
 func main() {
 
-	fmt.Println(base64Encode("AnyTextYouWantBase64Encoded"))
-}
+	resp:= []byte("\x00" + "raihanmahmudi35@gmail.com" + "\x00" + "your password")
 
-func base64Encode(strtxt string) (encodedStr string) {
-
-	//bnJqZ3pwcmlkcG14d3lreQ==
-	encodedStr = base64.StdEncoding.EncodeToString([]byte(strtxt))
-	return
-}
-
-func gmailPlainTextAuthCode() {
-
-	//for generating gmail plain text authentication
-	resp := []byte("\x00" + "raihanmahmudi35@gmail.com" + "\x00" + "raihan")
+	fmt.Println(string(resp), resp)
 	sEnc := base64.StdEncoding.EncodeToString([]byte(resp))
 	fmt.Println(sEnc)
-
 }
