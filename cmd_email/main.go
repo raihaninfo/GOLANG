2fc5d429267b70f128db755d3c07618939197e20
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-20 22:24:00 +0600

    code format

--- cmd_email/main.go
+++ cmd_email/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	resp:= []byte("\x00" + "raihanmahmudi35@gmail.com" + "\x00" + "your password")
+	resp := []byte("\x00" + "raihanmahmudi35@gmail.com" + "\x00" + "your password")
 
 	fmt.Println(string(resp), resp)
 	sEnc := base64.StdEncoding.EncodeToString([]byte(resp))
