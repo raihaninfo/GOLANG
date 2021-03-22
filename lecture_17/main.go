0da681618069efc46b6e9697722af9b72ef790b7
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-22 12:41:43 +0600

    about rout

--- lecture_17/main.go
+++ lecture_17/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 	http.ListenAndServe(":8888", nil)
 
 }
 
 func home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, `Welcome To my first golang Web page`)
+}
+func about(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, `Welcome To About Page`)
 }
