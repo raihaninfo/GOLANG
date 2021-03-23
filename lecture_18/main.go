191f20b1a9b5f897905d831cd4b72006561fe792
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-23 23:03:42 +0600

    database connection successful

--- lecture_18/main.go
+++ lecture_18/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+var err error
+
+func init() {
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err = sql.Open("mysql", "root:mysql@tcp(127.0.0.1:3306)/hosting")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+
+	//defer db.Close()
+	fmt.Println("database connection successfull")
+}
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/features", features)
@@ -52,14 +75,34 @@ func docs(w http.ResponseWriter, r *http.Request) {
 	ptmp.Execute(w, nil)
 }
 
-func request(w http.ResponseWriter, r *http.Request){
+func request(w http.ResponseWriter, r *http.Request) {
 
-	//r.ParseForm()
+	// Method 1
+
 	name := r.FormValue("name")
 	company := r.FormValue("company")
 	email := r.FormValue("email")
-	fmt.Println(name, company, email)
+	//fmt.Println(name, company, email)
+	//fmt.Fprintf(w, `received form %s %s %s`, name, company, email) //response
 
-	fmt.Fprintf(w, `received form %s %s %s`, name, company, email) //response 
+	//Method 2
+
+	// r.ParseForm()
+	// for key, val := range r.Form {
+	// 	fmt.Println(key, val)
+	// }
+	
+	qs:= "INSERT INTO `reqest` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+	sql:= fmt.Sprintf(qs, name, company, email)
+	insert, err := db.Query(sql)
+
+	if err != nil{
+		panic(err.Error())
+	}
+	
+	defer insert.Close()
+
+	fmt.Fprintf(w, `OK`)
+
 
 }
