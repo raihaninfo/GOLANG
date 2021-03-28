80f7ad9b04530c4900474305f90e7ac57a0316a9
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-28 20:13:22 +0600

    create txt file

---
+++ file_handling/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(dir)
+
+	posf, err := os.Create("raihan.txt")
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+
+	n, err:= posf.Write([]byte("My name is Raihan"))
+	fmt.Println(n, err)
+	
+	posf.Close()
+}
