867410d3130e28db00bcb96a84ccf715c7923d9e
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-04-06 23:05:24 +0600

    create file

--- file_handling/excelFileHandling/main.go
+++ file_handling/excelFileHandling/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	file, err := os.Create("index.html")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	file.Write([]byte("My name is raihan"))
+	defer file.Close()
 	fmt.Println("raihan")
 }
