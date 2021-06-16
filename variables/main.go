675ad52b7b6fca55485697f7217b58ddc394c61f
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-16 21:57:00 +0600

    Style: code format

--- variables/main.go
+++ variables/main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-//var num1 int = 12
-//var num2 int = 12
-//var sum int = num1+num2
-//var name string = "Raihan"
-var c rune = 'A'
-var age int = 10
-var result float32 =20.202
-fmt.Println(c, age, result)
+func main() {
+	//var num1 int = 12
+	//var num2 int = 12
+	//var sum int = num1+num2
+	//var name string = "Raihan"
+	var c rune = 'A'
+	var age int = 10
+	var result float32 = 20.202
+	fmt.Println(c, age, result)
 }
