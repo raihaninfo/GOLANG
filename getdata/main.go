5fb183f0140ba903aa2385fbb923e2df1347fe48
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-10 16:11:10 +0600

    err handeling

--- getdata/main.go
+++ getdata/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	resp, _ := http.Get("https://jsonplaceholder.typicode.com/posts")
-	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		fmt.Println(err)
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 	string_body := string(bytes)
 	fmt.Println(string_body)
 	resp.Body.Close()
