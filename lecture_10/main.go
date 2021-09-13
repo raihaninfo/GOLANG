633a892149246c7b0706cc76e0ad7138817aabdb
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-13 19:17:06 +0600

    lecture 10 update

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+<<<<<<< Updated upstream
 	// map, struct
 	x := make(map[string]string)
 	x["Name"] = "Raihan"
@@ -11,4 +12,22 @@ func main() {
 	delete(x, "Job")
 	fmt.Println(x)
 	// fmt.Println(x["Name"])
+=======
+	// array, slice, map, struct
+	var student [3]string
+	student[0] = "Raihan"
+	student[1] = "Hasan"
+	student[2] = "Ruhul"
+	fmt.Println(student[2])
+	var age [3]int
+	age[0] = 12
+	age[1] = 15
+	age[2] = 20
+	fmt.Println(age[2])
+	// fmt.Println(len(student))
+	// short hand way
+	students := [...]string{"Raihan", "hasan", "Ruhul-Amin", "monirul", "Rabbi"}
+	fmt.Println(students)
+
+>>>>>>> Stashed changes
 }
