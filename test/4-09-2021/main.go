5ff5184d8a0ab5bfa235bc4be84936716d7f892e
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-04 11:33:30 +0600

    array of sum

--- test/4-09-2021/main.go
+++ test/4-09-2021/main.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
- 
-
 func main() {
-	pragraph := "my name is raihan, i'm from bangladesh"
+	// search "M"
+	// pragraph := "my name is raihan, i'm from bangladesh"
+	// for _, value := range pragraph {
+	// 	valueString := string(value)
+	// 	if valueString == "m" {
+	// 		fmt.Println(valueString)
+	// 	}
+	// }
 
-	for _, value := range pragraph {
-		valueString := string(value)
-		if valueString == "m" {
-			fmt.Println(valueString)
-		}
+	// array of sum
+	numbers := [...]float64{1, 2, 3}
+	result := 0.0
+	for _, value := range numbers {
+		result += value
 	}
+	fmt.Println(result)
+
 }
