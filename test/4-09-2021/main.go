a931d4a8f7caaf9994dded6038a5692426ffa26b
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-04 22:19:47 +0600

    time package

--- test/4-09-2021/main.go
+++ test/4-09-2021/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	// search "M"
+	// // search "M"
 	// pragraph := "my name is raihan, i'm from bangladesh"
 	// for _, value := range pragraph {
 	// 	valueString := string(value)
@@ -12,12 +15,14 @@ func main() {
 	// 	}
 	// }
 
-	// array of sum
-	numbers := [...]float64{1, 2, 3}
-	result := 0.0
-	for _, value := range numbers {
-		result += value
-	}
-	fmt.Println(result)
+	// // array of sum
+	// numbers := [...]float64{1, 2, 3}
+	// result := 0.0
+	// for _, value := range numbers {
+	// 	result += value
+	// }
+	// fmt.Println(result)
 
+	todayTime:= time.Now()
+	fmt.Println(todayTime)
 }
