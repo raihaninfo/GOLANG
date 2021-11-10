e353988e8d5a2317fad75d3b714f56d4ec146266
raihaninfo <raihanmahmudi35@gmail.com>
2021-11-10 14:52:23 +0600

    golang login system

--- login_Session/main.go
+++ login_Session/main.go
@@ -2,20 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
-	"html/template"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
+var cookie *sessions.CookieStore
 
-func main()  {
-	mux := mux.NewRouter()
-	mux.HandleFunc("/", login)
-	fmt.Println("Listening port :8080")
-	http.ListenAndServe(":8080", mux)
+func init() {
+	cookie = sessions.NewCookieStore([]byte("Golang-Blogs"))
 }
 
-func login(w http.ResponseWriter, r *http.Request)  {
-	tmp,_:=template.ParseFiles("view/index.gohtml")
-	tmp.Execute(w, nil)
+func home(w http.ResponseWriter, r *http.Request) {
+	session, _ := cookie.Get(r, "Golang-session")
+	var authenticated interface{} = session.Values["authenticated"]
+	fmt.Fprintf(w, "Raihan\n")
+	
+	if authenticated != nil {
+		isAuthenticated := session.Values["authenticated"].(bool)
+		if !isAuthenticated {
+			http.Error(w, "UnAuthorized to Access this Page.", http.StatusForbidden)
+			return
+		}
+		fmt.Fprintf(w, "Authenticated User's Home Page")
+	}
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := cookie.Get(r, "Golang-session")
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+	fmt.Fprintf(w, "Successfully Logged In")
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := cookie.Get(r, "Golang-session")
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+	fmt.Fprintf(w, "Successfully Logged Out")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	fmt.Println("Listening port :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error Starting the HTTP Server : ", err)
+		return
+	}
 }
